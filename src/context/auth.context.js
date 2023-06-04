59a4f0e17e695ba0932cb955f77b05356d70ce0b
Zefanja Timorason <z.timorason@gmail.com>
2023-06-04 14:40:55 +0200

    created a context to login

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -3,3 +3,63 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import config from "../config/config";
+
+const API_URL = config.API_URL;
+
+export const AuthContext = React.createContext();
+
+export default function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
+  };
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/auth/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          const user = response.data;
+          console.log("This is the user that is getting verified", user);
+          setIsLoggedIn(true);
+          setUser(user);
+        })
+        .catch((error) => {
+          setIsLoggedIn(false);
+          setUser(null);
+          console.log("There is an error verifying the user", error);
+        });
+    } else {
+      setIsLoggedIn(false);
+      setUser(null);
+    }
+  };
+  const removeToken = () => {
+    localStorage.removeItem("authToken");
+  };
+  const logOutUser = () => {
+    removeToken();
+    authenticateUser();
+    navigate("/");
+  };
+
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, user, storeToken, authenticateUser, logOutUser }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
