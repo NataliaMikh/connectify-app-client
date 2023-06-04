45845f6fb20f705a1113ff3e77d947c80c67e51c
Zefanja Timorason <z.timorason@gmail.com>
2023-06-04 15:01:50 +0200

    I forgot to add the route to the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/auth/signup" element={<SignUpPage />} />
       </Routes>
     </div>
   );
